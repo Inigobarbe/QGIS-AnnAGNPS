dfb01e7fe83d0669ca672867ec80672791ef961d
Iñigo Barberena Ruiz <inigo.barberena@unavarra.es>
2023-10-20 13:00:20 +0200

    20/10/2023

--- Executables/EjecutarTopagnps.bat
+++ Executables/EjecutarTopagnps.bat
@@ -1,2 +1,2 @@
-CD D:/Latxaga/TOPAGNPS 
+CD D:/borrar 
 CALL C:\Users/inigo.barberena/AppData/Roaming/QGIS/QGIS3\profiles\default/python/plugins\qannagnps\Executables\TopAGNPS_v6.00.a.020_release_64-bit.exe 
